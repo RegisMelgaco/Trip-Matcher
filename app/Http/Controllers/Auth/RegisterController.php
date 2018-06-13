d6b29e3ded1ac56b72ee9a33fd3716b0e874b5b2
Régis <regis.melgaco@gmail.com>
2018-06-13 11:25:41 -0300

    fix redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'Trips';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
